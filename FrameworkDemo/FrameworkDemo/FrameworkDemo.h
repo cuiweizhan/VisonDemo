8b5fcb9a8efce929e9748561706aab14248db941
VisonCui <vison@VisonCuideMacBook-Pro.local>
2018-03-16 11:15:37 +0800

    添加demo

---
+++ FrameworkDemo/FrameworkDemo/FrameworkDemo.h
@@ -0,0 +1,20 @@
+//
+//  FrameworkDemo.h
+//  FrameworkDemo
+//
+//  Created by VisonCui on 2018/3/14.
+//  Copyright © 2018年 VisonCui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FrameworkDemo.
+FOUNDATION_EXPORT double FrameworkDemoVersionNumber;
+
+//! Project version string for FrameworkDemo.
+FOUNDATION_EXPORT const unsigned char FrameworkDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkDemo/PublicHeader.h>
+
+#import <FrameworkDemo/BaseMethod.h>
+
